28e177ea659dc9181247c0d427e870cd4d6e8126
eddyhakimi <eddi.hakimi@gmx.de>
2021-11-09 20:50:53 +0100

    Download and install vim plugins through init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,7 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+# install the vim plugins
+git submodule init
+git submodule update
