394338d652619039ebaa52330d12205ec91ea7bc
eddyhakimi <eddi.hakimi@gmx.de>
2022-03-25 16:14:07 +0100

    Create symlinks independent of execution location

--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+# link system dotfiles to dotfiles in this repository 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  )
+YCM_INSTALL_SCRIPT="$SCRIPT_DIR/.vim/pack/vendor/start/YouCompleteMe/install.py"
+
+ln -s "$SCRIPT_DIR/.bashrc" ~/.bashrc
+ln -s "$SCRIPT_DIR/.tmux.conf" ~/.tmux.conf
+ln -s "$SCRIPT_DIR/.vim" ~/.vim
+ln -s "$SCRIPT_DIR/.vimrc" ~/.vimrc
 
 # install the vim plugins
-git submodule init
-git submodule update
+git submodule update --init --recursive
+python3 "$YCM_INSTALL_SCRIPT" --all
+
