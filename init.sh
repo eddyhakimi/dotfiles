aaf257ae365718e8909e24f98baa923396980916
eddyhakimi <eddy.hakimi@gmx.de>
2022-12-11 00:14:36 +0100

    Move existing dotfiles on init into tmp directoy

--- init.sh
+++ init.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-
-# link system dotfiles to dotfiles in this repository 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  )
 
+# Move existing dotfiles into temporary directory
+mkdir -p tmp
+handle_existing_dotfile () {
+  if [ -f "$1" -a ! -h "$1" ]; then
+      echo "$1 already exists. Moving $1 to $SCRIPT_DIR/tmp"
+      mv $1 $SCRIPT_DIR/tmp/
+  fi
+}
+
+FILE=~/.bashrc
+handle_existing_dotfile $FILE
+FILE=~/.tmux.conf
+handle_existing_dotfile $FILE
+FILE=~/.vim
+handle_existing_dotfile $FILE
+FILE=~/.vimrc
+handle_existing_dotfile $FILE
+
+# link system dotfiles to dotfiles in this repository 
 ln -s "$SCRIPT_DIR/.bashrc" ~/.bashrc
 ln -s "$SCRIPT_DIR/.tmux.conf" ~/.tmux.conf
 ln -s "$SCRIPT_DIR/.vim" ~/.vim
